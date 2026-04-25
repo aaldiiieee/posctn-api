cd8ffd2dd5b5ba016cb0905d388d317bacadd93e
aaldiiieee <prmdy02@gmail.com>
2026-04-25 10:31:02 +0700

    feat(config): add CORS config

---
+++ src/main/java/posctn/posctn_api/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package posctn.posctn_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply to all paths
+                .allowedOrigins("http://localhost:8082") // Approved origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600); // Cache preflight response for 1 hour
+    }
+}
+
